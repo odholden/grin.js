72926068fb2dcd106316b6045e23a2b6a2c7281c
odholden <odholden@gmail.com>
2016-02-07 19:39:38 +0000

    added custom scroll listeners and object orientations

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
-window.addEventListener("DOMContentLoaded", init);
+$(init);
 
 function init() {
-  // loopText(loops);
-  var ollie = new Ollie();
-}
-
-function Ollie() {
-  this.loops = {
+  loops = {
     serious: ["Coding Teacher", "Full-Stack Developer"],
     other:   ["Beanies", "Bannoffee Pie", "Burgers", "Fentiman's Ginger Beer",
     "Hitchhiking", "Hot Tubs", "Indie Movies", "Backpacking", "The Economist",
     "Old Cars", "Australia", "Teaching", "Coding", "Theatre"]
   };
+  var site = new Site(loops);
+  site.checkScroll();
+  // loopText();
+}
+
+function Site(loops) {
+  this.loops         = loops;
+  this.currentScroll = 0;
+  this.newScroll;
+}
+
+Site.prototype.checkScroll = function() {
+  $(window).on("scroll", this.detectScroll.bind(this));
+}
+
+Site.prototype.detectScroll = function() {
+  this.newScroll = $(window).scrollTop();
+  if (this.newScroll > this.currentScroll) {
+    console.log("scroll down");
+  } else {
+    console.log("scroll up");
+  }
+  this.currentScroll = this.newScroll;
 }
