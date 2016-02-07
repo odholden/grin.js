6a3fbff25342a43bca65f4712f23c1cd656ce5df
odholden <odholden@gmail.com>
2016-02-07 16:42:24 +0000

    began object oriented text loop

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  console.log("init");
-  loopText(loop);
+  // loopText(loops);
+  var ollie = new Ollie();
+}
+
+function Ollie() {
+  this.loops = {
+    serious: ["Coding Teacher", "Full-Stack Developer"],
+    other:   ["Beanies", "Bannoffee Pie", "Burgers", "Fentiman's Ginger Beer",
+    "Hitchhiking", "Hot Tubs", "Indie Movies", "Backpacking", "The Economist",
+    "Old Cars", "Australia", "Teaching", "Coding", "Theatre"]
+  };
 }
